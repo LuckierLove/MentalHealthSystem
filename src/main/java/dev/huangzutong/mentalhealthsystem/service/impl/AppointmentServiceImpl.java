a705b7e042a8c7bdfd99108cfcf18550174e5d57
月饮风 <tanchengzhang520@163.com>
2025-12-07 23:12:32 +0800

    feat(appointment): 添加咨询师评分更新功能
    - 在预约服务中引入咨询师映射器
    - 实现预约更新时同步更新咨询师评分
    - 添加事务注解确保数据一致性
    - 优化密码加密逻辑，仅在非空时进行BCrypt加密

--- src/main/java/dev/huangzutong/mentalhealthsystem/service/impl/AppointmentServiceImpl.java
+++ src/main/java/dev/huangzutong/mentalhealthsystem/service/impl/AppointmentServiceImpl.java
@@ -6,16 +6,20 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import dev.huangzutong.mentalhealthsystem.entity.Appointment;
+import dev.huangzutong.mentalhealthsystem.entity.Counselor;
 import dev.huangzutong.mentalhealthsystem.entity.req.CreateAppointmentReq;
 import dev.huangzutong.mentalhealthsystem.entity.vo.GetListVO;
 import dev.huangzutong.mentalhealthsystem.mapper.AppointmentMapper;
+import dev.huangzutong.mentalhealthsystem.mapper.CounselorMapper;
 import dev.huangzutong.mentalhealthsystem.service.IAppointmentService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import jakarta.annotation.Resource;
 import org.springframework.beans.BeanUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 /**
@@ -26,6 +30,8 @@ public class AppointmentServiceImpl extends ServiceImpl<AppointmentMapper, Appoi
 
     @Resource
     private AppointmentMapper appointmentMapper;
+    @Resource
+    private CounselorMapper counselorMapper;
 
     /**
      * 添加预约
@@ -50,10 +56,18 @@ public class AppointmentServiceImpl extends ServiceImpl<AppointmentMapper, Appoi
      * @param id 预约id
      * @param appointment 更新预约请求参数
      */
+    @Transactional
     @Override
     public void updateAppointment(Long id, Appointment appointment) {
         appointment.setId(id);
         updateById(appointment);
+
+        // 更新对应咨询师评分
+        if(appointment.getRating() != null){
+            Counselor counselor = counselorMapper.selectOne(new QueryWrapper<Counselor>().eq("user_id", appointmentMapper.selectById(id).getCounselorId()));
+            counselor.setRating((counselor.getRating().add(BigDecimal.valueOf(appointment.getRating())).divide(BigDecimal.valueOf(2))));
+            counselorMapper.updateById(counselor);
+        }
     }
 
     /**
